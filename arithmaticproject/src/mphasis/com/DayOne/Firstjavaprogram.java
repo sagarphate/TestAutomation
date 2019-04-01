29c28ffd499a085289a3f2cce885ef4a5e53d3a3
sphate <sphate@IN5CD7031HKX.CSCMWS.CSCMWS.COM>
2019-04-02 01:21:09 +0530

    this is math project

---
+++ arithmaticproject/src/mphasis/com/DayOne/Firstjavaprogram.java
@@ -0,0 +1,23 @@
+package mphasis.com.DayOne;
+
+public class Firstjavaprogram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is my Frist Java Program");
+		System.out.println("This is Java Class");
+		
+		int a = 10;
+		double d = 5.55;
+		char c = 'S';
+		boolean b = false;
+		String name = "Sagar";
+		
+		System.out.println(a);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(b);
+		System.out.println(name);
+	
+	}
+}
---
+++ arithmaticproject/src/mphasis/com/DayOne/Operators.java
@@ -0,0 +1,19 @@
+package mphasis.com.DayOne;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		
+		int a = 10, b = 20;
+		
+		// Arithmetic Operators
+		System.out.println("Addition of a and b = " +(a+b));
+		System.out.println("Mul of a and b = " +(a*b));
+		System.out.println("Dev of a and b = " +(a/b));
+		System.out.println("Subtraction of a and b = " +(a-b));
+		System.out.println("Mod of a and b = " +(a%b));
+	
+	
+	}
+
+}
